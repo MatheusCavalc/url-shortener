b96dcb786d816af5eee6b00da6f56547537872eb
Matheus Cavalcante <matheuscavalcs@gmail.com>
2023-05-03 19:33:11 -0300

    add home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,8 +3,19 @@ import MainLayout from '@/layouts/MainLayout.vue';
 </script>
 
 <template>
-<MainLayout>
-  <h1 class="font-bold">Oi</h1>
-</MainLayout>
+  <MainLayout>
+    <div class="border-gray-200 dark:border-gray-700 grid grid-cols-5 gap-4 place-content-center h-screen">
+      <div class="col-span-3">
+        <p class="text-4xl font-bold">We’ve expanded!</p>
+        <p class="text-4xl font-bold">Shorten URLs. Generate QR Codes. </p>
+        <p class="text-4xl font-bold">And now, create Link-in-bios.</p>
+      </div>
+
+      <div class="text-center col-span-2">
+        <img class="h-auto w-auto" src="https://docrdsfx76ssb.cloudfront.net/static/1683058957/pages/wp-content/uploads/2022/10/homepage-hero-a-bit-closer-desktop-v2.png" alt="">
+      </div>
+
+    </div>
+  </MainLayout>
 </template>
 
